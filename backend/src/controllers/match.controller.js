46123c5541906e35ef2cea689b8b9a55bac201d4
aasmaa01 <asmaesoltaniii@gmail.com>
2025-12-10 12:52:41 +0100

    Implement match and player controllers with error handling and response formatting

---
+++ backend/src/controllers/match.controller.js
@@ -0,0 +1,159 @@
+import { MatchService } from "../services/match.service";
+
+
+export const getAllMatches= async (req,res) => {
+    try {
+        const match= await MatchService.getAllMatches();
+        return res.json({
+            success: true,
+            data: match
+        });
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        })
+    }
+}
+
+
+export const getMatchById= async (req,res) => {
+    try {
+        const {id}=req.params;
+        if (!id) {
+            return res.status(400).json({
+                success: false,
+                message:"Id match is required"
+            })
+        }
+        const match= await MatchService.getMatchById(id);
+        if (!match) {
+            return res.status(400).json({
+                success: false,
+                message:" Match not found"
+            })
+        }
+        res.json({
+            success: true,
+            data: match
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        })
+    }
+}
+
+export const getUpcomingMaches= async (req,res) => {
+    try {
+        const match= await MatchService.getUpcomingMaches()
+        if (!match) {
+            return res.status(400).json({
+                success: false,
+                message:"No match found"
+            })            
+        }
+
+        res.json({
+            success: true,
+            data: match
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        })
+    }
+}
+
+
+export const getFinishedMatches= async (req,res) => {
+    try {
+        const match= await MatchService.getUpcomingMaches()
+        if (!match) {
+            return res.status(400).json({
+                success: false,
+                message:"No match found"
+            })            
+        }
+        
+        res.json({
+            success: true,
+            data: match
+        })    
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        })
+    }
+}
+
+
+export const getMatchDetails= async (req,res) => {
+    try {
+        const {id}=req.params
+        if (!id) {
+            return res.status(400).json({
+                success: false,
+                message:"Id match is required"
+            })            
+        }
+        const match = await MatchService.getMatchDetails(id);
+
+        if (!match) {
+            return res.status(400).json({
+                success: false,
+                message: ""
+            })            
+        }
+        res.json({
+            success:true,
+            data: match
+        })
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        })
+    }
+}
+
+
+export const getMatchEvents= async (req,res) => {
+    try {
+        const {id}= req.params
+        if (!id) {
+            return res.status(400).json({
+                success: false,
+                message: "Id match is required"
+            })
+        }
+        const matchEvent=await MatchService.getMatchEvents(id);
+        if (!matchEvent) {
+            return res.status(400).json({
+                success: false,
+                message: "No event found in this match!"
+            })
+        }
+
+        res.json({
+            success: true,
+            data: matchEvent
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        })
+    }
+}
---
+++ backend/src/controllers/team.controller.js
@@ -0,0 +1,107 @@
+import { TeamService } from "../services/team.service";
+
+export const getAllTeams=async (req,res)=>{
+    try{
+        const teams=await TeamService.getAllTeams();
+        res.json({
+            success:true,
+            data:teams
+        });
+    }catch(error){
+        res.status(500).json({
+            success:false,
+            message:"Internal Server Error"});
+    }
+};
+
+export const getTeamById=async (req,res)=>{
+    try{
+        const {id}=req.params;
+        if (!id){
+            return res.status(400).json({
+                succes: false,
+                message: "Team ID is required"
+            })
+        }
+        const team=await TeamService.getTeamById(id); 
+        if(!team){
+            return res.status(400).json({
+                success:false,
+                message:"Team not found!"
+            })
+        }
+
+        res.json({
+            success:true,
+            data:team
+        });
+
+    }
+    catch(error){
+        console.error(error);
+        res.status(500).json({
+            success:false,
+            message:"Internal Server Error"});
+    }
+};
+
+export const getTeamPlayers=async(req,res)=>{
+    try{
+        const {id}=req.params;
+        if(!id){
+            return res.status(400).json({
+                success:false,
+                message:"Team ID is required"
+            });
+        }
+        const players=await TeamService.getTeamPlayers(id);
+        if(players.length===0){
+            return res.status(404).json({
+                success:false,
+                message:"No players found for this team"
+            });
+        }
+
+        res.json({
+            success:true,
+            data:players
+        });
+    }
+    catch(error){
+        console.error(error);
+        res.status(500).json({
+            success:false,
+            message:"Internal Server Error"});
+    }
+}
+
+export const getTeamMatches=async(req,res)=>{
+    try {
+        const {id}=req.params;
+        if (!id) {
+            return res.status(400).json({
+                success:false,
+                message:"Team ID is required"
+            });
+
+        }
+        const matches= await TeamService.getTeamMaches(id);
+        if (matches.length===0) {
+            return res.status(404).json({
+                success:false,
+                message:"No matches found for this team"
+            });
+        }
+
+        res.json({
+            success:true,
+            data:matches
+        });
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            success:false,
+            message:"Internal Server Error"});
+    }
+}
