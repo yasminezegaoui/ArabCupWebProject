8dec46b74d005ac017d252e7056b6f96107b1313
yasminezegaoui <yasminezegaoui1@gmail.com>
2025-12-12 13:54:05 +0100

    added routes, fixed folder structure, configured tailwindcss

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const DashboardLayout = () => {
+const Footer = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const DashboardLayout = () => {
   )
 }
 
-export default DashboardLayout
+export default Footer
