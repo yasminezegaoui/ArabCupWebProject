48f4aa11960da8f05f7d45171701aadf020a6841
aasmaa01 <asmaesoltaniii@gmail.com>
2025-12-12 02:24:10 +0100

    remove parentheses

--- backend/src/routes/match.routes.js
+++ backend/src/routes/match.routes.js
@@ -7,7 +7,7 @@ router.get("/", getAllMatches);
 router.get("/:id", getMatchById);
 router.get("/upcoming", getUpcomingMaches);
 router.get("/finished", getFinishedMatches);
-router.get("/:id/details", getMatchDetails());
-router.get("/:id/events", getMatchEvents());
+router.get("/:id/details", getMatchDetails);
+router.get("/:id/events", getMatchEvents);
 
 export default router;
