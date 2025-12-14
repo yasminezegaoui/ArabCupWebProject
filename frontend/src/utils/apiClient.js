ef86cf22415c61b33f42d2674c1ff177a12eabaf
aasmaa01 <asmaesoltaniii@gmail.com>
2025-12-14 20:39:55 +0100

    save local change

--- frontend/src/utils/apiClient.js
+++ frontend/src/utils/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost', //i change it so NGINX decides where traffic goes
     headers:{
         'Content-Type': 'application/json'
     }
